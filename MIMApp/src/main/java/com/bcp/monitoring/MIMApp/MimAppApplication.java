04ee50b5ad377bb83905c0bef47f2b67f1c5fd3a
Bettel01 <ibtissamelmouhtaram0@gmail.com>
2024-05-23 15:54:08 +0100

    Initial commit.

---
+++ MIMApp/src/main/java/com/bcp/monitoring/MIMApp/MimAppApplication.java
@@ -0,0 +1,13 @@
+package com.bcp.monitoring.MIMApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimAppApplication.class, args);
+	}
+
+}
---
+++ MIMApp/src/test/java/com/bcp/monitoring/MIMApp/MimAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcp.monitoring.MIMApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
